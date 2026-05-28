52bbb56c4ec6d8d053a9d6d2513af653badd7689
vhego010467-dev <vhego010467@gmail.com>
2026-05-28 13:19:10 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,23 +1,49 @@
+```javascript id="v7u2lx"
 import pesertaDB from "../data/peserta-db.js";
 
 const form = document.getElementById("loginForm");
 
+const statusBox =
+  document.getElementById("statusBox");
+
 form.addEventListener("submit", function (e) {
+
   e.preventDefault();
 
-  const id = document.getElementById("pesertaId").value.trim();
-  const password = document.getElementById("password").value.trim();
+  const id = document
+    .getElementById("pesertaId")
+    .value
+    .trim()
+    .toUpperCase();
+
+  const password = document
+    .getElementById("password")
+    .value
+    .trim();
 
   if (
     pesertaDB[id] &&
     pesertaDB[id].password === password
   ) {
-    alert("Login berhasil");
 
-    // redirect tujuan ujian
-    window.location.href =
-      "https://sites.google.com/view/final-exam-v2025/HALAMAN-IOS?authuser=1";
+    statusBox.innerHTML =
+      "Login berhasil. Mengalihkan ke halaman ujian...";
+
+    // delay kecil anti spam / anti double click
+    setTimeout(() => {
+
+      window.location.replace(
+        "https://sites.google.com/view/final-exam-v2025/HALAMAN-IOS?authuser=1"
+      );
+
+    }, 1200);
+
   } else {
-    alert("ID atau password salah");
+
+    statusBox.innerHTML =
+      "ID Peserta atau Password salah.";
+
   }
+
 });
+```
